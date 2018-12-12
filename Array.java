2fb946fc30c8cb711d4a600dfae6fb2c18c27743
san1619 <45599619+san1619@users.noreply.github.com>
2018-12-12 15:28:29 -0500

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,20 @@
+package assign;
+import java.util.Scanner;
+
+public class Array {
+	public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] array = new int[scan.nextInt()];
+        for(int i = 0; i < array.length; i++){
+            array[i] = scan.nextInt();
+        }
+        scan.close();
+        
+        for(int i = array.length - 1; i >= 0; i--){
+            System.out.print(array[i] + " ");
+        }
+    }
+}
+
+	     	  
+	   
